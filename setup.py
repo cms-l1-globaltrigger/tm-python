a1ea1a02b69bf3dbd2e17be5c8bfea6c3cd93d00
Bernhard Arnold <arnobaer@users.noreply.github.com>
2021-12-03 08:25:48 +0100

    check installed deps

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import sys
 from setuptools import setup
 
 UTM_VERSION = '0.9.1'
+BASE_URL = 'https://github.com/cms-l1-globaltrigger'
 
 def _version():
     """Returns major and minor interpreter version."""
-    return "{}{}".format(sys.version_info[0], sys.version_info[1])
+    return '{}{}'.format(sys.version_info[0], sys.version_info[1])
 
 def _platform():
     """Returns platform signature for wheels."""
@@ -15,8 +16,37 @@ def _platform():
         return 'darwin'
     raise ValueError("Platform not supported: {}".format(sys.platform))
 
-# Note: no more 'm' abiflag since Python 3.8.0
-WHEEL_SIGNATURE = 'cp{0}-cp{0}{2}-{1}_x86_64'.format(_version(), _platform(), sys.abiflags)
+def _signature():
+    # Note: no more 'm' abiflag since Python 3.8
+    return 'cp{0}-cp{0}{1}-{2}_x86_64'.format(_version(), sys.abiflags, _platform())
+
+def _wheel_name(name, version):
+    basename = name.replace('-', '_')
+    return '{0}-{1}-{2}.whl'.format(basename, version, _signature())
+
+def _require(name, version):
+    filename = _wheel_name(name, version)
+    return f'{0} @ {BASE_URL}/{0}/releases/download/{1}/{2}'.format(name, version, filename)
+
+install_requires = []
+
+try:
+    from tmTable import __version__
+    assert __version__ == UTM_VERSION
+except ModuleNotFoundError, ImportError, AssertionError:
+    install_requires.append(_require('tm-table', UTM_VERSION))
+
+try:
+    from tmGrammar import __version__
+    assert __version__ == UTM_VERSION
+except ModuleNotFoundError, ImportError, AssertionError:
+    install_requires.append(_require('tm-grammar', UTM_VERSION))
+
+try:
+    from tmEventSetup import __version__
+    assert __version__ == UTM_VERSION
+except ModuleNotFoundError, ImportError, AssertionError:
+    install_requires.append(_require('tm-eventsetup', UTM_VERSION))
 
 setup(
     name='tm-python',
@@ -24,10 +54,6 @@ setup(
     author="Bernhard Arnold",
     author_email="bernhard.arnold@cern.ch",
     description="""Meta package providing Python bindings for tmTable, tmGrammar and tmEventSetup""",
-    install_requires=[
-        'tm-table @ https://github.com/cms-l1-globaltrigger/tm-table/releases/download/{0}/tm_table-{0}-{1}.whl'.format(UTM_VERSION, WHEEL_SIGNATURE),
-        'tm-grammar @ https://github.com/cms-l1-globaltrigger/tm-grammar/releases/download/{0}/tm_grammar-{0}-{1}.whl'.format(UTM_VERSION, WHEEL_SIGNATURE),
-        'tm-eventsetup @ https://github.com/cms-l1-globaltrigger/tm-eventsetup/releases/download/{0}/tm_eventsetup-{0}-{1}.whl'.format(UTM_VERSION, WHEEL_SIGNATURE)
-    ],
+    install_requires=install_requires,
     license="GPLv3"
 )
